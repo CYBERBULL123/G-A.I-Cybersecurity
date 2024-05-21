224ea700bac0c7235d2d0d243c0055602d57d5a2
Aditya Pandey <126447573+CYBERBULL123@users.noreply.github.com>
2024-05-21 17:06:37 +0530

    Update RAG.py

--- RAG.py
+++ RAG.py
@@ -11,11 +11,6 @@ from bs4 import BeautifulSoup
 import urllib.request
 from google.api_core.exceptions import GoogleAPIError
 
-def load_css(file_name):
-    with open(file_name) as f:
-        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
-# Load the CSS file
-load_css("ui/Style.css")
 
 # Streamlit configuration
 st.set_page_config(
@@ -23,6 +18,12 @@ st.set_page_config(
     page_icon="🎨",
     layout="wide"
 )
+
+def load_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+# Load the CSS file
+load_css("ui/Style.css")
 
 # API configuration
 os.environ["GOOGLE_API_KEY"] = gemini_key
