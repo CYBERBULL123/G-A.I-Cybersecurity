88842e9071baec6452fb9703b910284aee20d33c
Aditya Pandey <126447573+CYBERBULL123@users.noreply.github.com>
2024-05-21 16:28:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -87,7 +87,7 @@ def get_gemini_response(input_prompt, image):
     return response.text
 
 def render_main_program():
-    st.header("🔒 Unlock the Future of Cybersecurity with OxSecure: Where Knowledge Meets Innovation! 🚀 Dive into Cyber Brilliance with OxSecure 🤖 🌟")
+    st.markdown(" # 🔒 Unlock the Future of Cybersecurity with OxSecure , ### Where Knowledge Meets Innovation! , ***🚀 Dive into Cyber Brilliance with OxSecure** 🤖 🌟")
     st.markdown("----")
     app_choice = st.sidebar.radio("Choose App", ("OxSecure Chat 🤖", "OxSecure ImaGen 🎨"))
 
