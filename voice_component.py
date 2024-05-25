eec2cdf24a0a78b89623b21349136dd3d9a33484
Aditya Pandey <126447573+CYBERBULL123@users.noreply.github.com>
2024-05-25 09:40:01 +0530

    voice_component.py

--- voice_component.py
+++ voice_component.py
@@ -3,7 +3,7 @@ import streamlit.components.v1 as components
 # Declare the component ( HTML file is in a 'public' folder)
 voice_recognition_component = components.declare_component(
     "voice_recognition",
-    path="public/voice_recognition.html"
+    path="public"
 )
 
 def get_voice_input():
