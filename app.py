eaa1b6e7f5d4697bac3634a7e5bf397033393f55
Aditya Pandey <126447573+CYBERBULL123@users.noreply.github.com>
2024-09-14 12:39:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -199,9 +199,8 @@ def features():
         """)
 # Help and Use Section
 def use_app():
-    st.write("***📋 How to Use OxSecure Intelligence***")
     st.write("""
-     🚀 **OxSecure Intelligence: Use Cases**
+     🚀 ***OxSecure Intelligence: Use Cases***
 
 OxSecure Intelligence is a comprehensive cybersecurity tool designed to provide in-depth information on various security topics, analyze images, and perform detailed file analysis. The app consists of three powerful tools:
 
